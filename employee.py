600c4e62c2fbc7d59be8676b0d03bf47019b1524
Eve Ster <paraskevi.stergiopoulou@kcl.ac.uk>
2022-10-12 22:06:06 +0100

    Exercise 1.5 completed

--- employee.py
+++ employee.py
@@ -1,31 +1,115 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
+from abc import ABC, abstractmethod
 
 class Employee:
-    def __init__(self, name):
+    def __init__(self, name, salary, commission):
         self.name = name
+        self.salary = salary
+        self.commission = commission
 
+    def get_pay(self):
+        return self.salary.get_pay() + self.commission.get_pay()
+
+    def __str__(self):
+        return (f'{self.name} works on a {self.salary.__str__()}{self.commission.__str__()}. Their total pay is {self.get_pay()}.')
+
+#Salary - Interface
+class Salary(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
     def get_pay(self):
         pass
 
+    @abstractmethod
     def __str__(self):
-        return self.name
+        pass
 
+#Salary - Concrete classes   
+class HourlySalary(Salary):
+    def __init__(self, hourly_wage, hours_worked):
+        self.hourly_wage = hourly_wage
+        self.hours_worked = hours_worked
+
+    def get_pay(self):
+        return self.hourly_wage * self.hours_worked
+
+    def __str__(self):
+        return (f'contract of {self.hours_worked} hours at {self.hourly_wage}/hour')
+
+class MonthlySalary(Salary):
+    def __init__(self, monthly_wage):
+        self.monthly_wage = monthly_wage
+
+    def get_pay(self):
+        return self.monthly_wage
+
+    def __str__(self):
+        return (f'monthly salary of {self.monthly_wage}')
+
+#Commission - Interface
+class Commission(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_pay(self):
+        pass
+        
+    @abstractmethod
+    def __str__(self):
+        pass
+
+#Commission - Concrete classes
+class NoCommission(Commission):
+    def __init__(self):
+        pass
+    
+    def get_pay(self):
+        return 0
+    
+    def __str__(self):
+        return ''
+
+class BonusCommission(Commission): 
+    def __init__(self, fixed_bonus):
+        self.fixed_bonus = fixed_bonus
+
+    def get_pay(self):
+        return self.fixed_bonus
+
+    def __str__(self):
+        return (f' and receives a bonus commission of {self.fixed_bonus}')
+
+class ContractCommission(Commission):
+    def __init__(self, contracts_landed, commission_per_contract):
+        self.contracts_landed = contracts_landed
+        self.commission_per_contract = commission_per_contract
+
+    def get_pay(self):
+        return self.contracts_landed * self.commission_per_contract
+
+    def __str__(self):
+        return (f' and receives a commission for {self.contracts_landed} contract(s) at {self.commission_per_contract}/contract')
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = Employee('Billie', MonthlySalary(4000), NoCommission())
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+charlie = Employee('Charlie', HourlySalary(25, 100), NoCommission())
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+renee = Employee('Renee', MonthlySalary(3000), ContractCommission(4, 200))
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+jan = Employee('Jan', HourlySalary(25, 150), ContractCommission(3, 220))
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+robbie = Employee('Robbie', MonthlySalary(2000), BonusCommission(1500))
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+ariel = Employee('Ariel', HourlySalary(30, 120), BonusCommission(600))
